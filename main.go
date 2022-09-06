52060c4c536efbf36eddaf439b7c99a56125cf4b
mdestafadilah <desta.08b@gmail.com>
2022-09-06 21:47:33 +0700

    Routing Hello

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hai", haiHandler)
+
+	log.Println("Menjalankan server web")
+
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
+
+func haiHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hai, saya belajar golang"))
 }
